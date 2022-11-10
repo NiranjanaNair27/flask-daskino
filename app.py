7b8717dd665cffa839a7d8ea6237aaa56d5f9bfe
Ghost4681 <64606908+Ghost4681@users.noreply.github.com>
2022-11-11 01:02:28 +0530

    its working till movies

--- app.py
+++ app.py
@@ -11,14 +11,22 @@ db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:tarun123@localhost/daskino'
 app.config['SECRET_KEY'] = 'awwfaw'
 
+#general functions
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('UNAUTHORISED, Please Login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
 # routes
-@app.route('/')
-def login():
-    return render_template('login.html')
 
+@is_logged_in
 @app.route('/home')
-def index():
-    return render_template('homepage.html')
+def home():
+    return render_template('homepage.html', movie = db.session.query(movies).order_by(movies.rating.desc()).all(), movie_len=db.session.query(movies).order_by(movies.rating.desc()).count() )
 
 @app.route('/about')
 def about():
@@ -28,46 +36,42 @@ def about():
 def admin():
     return render_template('admin.html')
 
-@app.route('/bookingtheatre')
-def bookingtheatre():
+@app.route('/theatre/<mname>')
+def theatre():
+    movie = db.session.query(Movie).filter(CustomerDet.username == username).first()
+
     return render_template('bookingtheatre.html')
 
 @app.route('/contact')
 def contact():
     return render_template('contactus.html')
 
-# @app.route("/login", methods = ['GET', 'POST'])
-# def logincustomer():
-#     if request.method == 'POST':
-#         usercust = request.form['username']
-#         password_candidate = request.form['password']
-#         user = db.session.query(CustomerDet).filter(CustomerDet.username == usercust).first()
-#         db.session.commit()
-#         if user is None:
-#             flash('No such username exists', 'danger')
-#             return render_template('login.html')
-#         else:
-#             if sa.verify(password_candidate, user.password):
-#                 session['logged_in'] = True
-#                 session['username'] = usercust
-#                 session['type'] = 'C'
-#                 session['name'] = user.namecust
-#                 session['pincode'] = user.pincode
-#                 flash('You are now logged in', 'success')
-#                 return redirect(url_for('home'))
-#             else:
-#                 flash('Incorrect password', 'danger')
-#                 return render_template('locust.html')
-#     else:
-#         return render_template('login.html')
+@app.route("/", methods = ['GET', 'POST'])
+def logincustomer():
+    if request.method == 'POST':
+        usercust = request.form['username']
+        password_candidate = request.form['password']
+        user = db.session.query(UserDet).filter(UserDet.username == usercust).first()
+        db.session.commit()
+        if user is None:
+            flash('No such username exists', 'danger')
+            return render_template('login.html')
+        else:
+            if password_candidate == user.passwd:
+                session['logged_in'] = True
+                session['username'] = usercust
+                session['name'] = user.namecust
+                flash('You are now logged in', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Incorrect password', 'danger')
+                return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 @app.route('/offers')
 def offers():
     return render_template('offers.html')
-
-# @app.route('/trans')
-# def trans():
-#     return render_template('transactions.html')
 
 def is_logged_in(f):
     @wraps(f)
@@ -145,19 +149,19 @@ class movies(db.Model):
         self.rating=rating
         self.poster=poster
 
-# class theatres(db.Model):
-#     __tablename__='theatres'
-#     theatreid=db.Column(db.Integer, primary_key= True)
-#     theatrename=db.Column(db.String(200))
-#     morning_show=db.Column(db.String(200), db.ForeignKey('movies.mname'))
-#     movies= db.relationship('movies',backref= backref("movies",  uselist=False))
-#     evening_show=db.Column(db.String(200))
+class theatres(db.Model):
+    __tablename__='theatres'
+    theatreid=db.Column(db.Integer, primary_key= True)
+    theatrename=db.Column(db.String(200))
+    morning_show=db.Column(db.String(200), db.ForeignKey('movies.mname'))
+    movies= db.relationship('movies',backref= backref("movies",  uselist=False))
+    evening_show=db.Column(db.String(200))
 
-    # def __init__(self, theatrename, morning_show, movies, evening_show):
-    #     self.theatrename = theatrename
-    #     self.morning_show = morning_show
-    #     self.movies = movies
-    #     self.evening_show = evening_show
+    def __init__(self, theatrename, morning_show, movies, evening_show):
+        self.theatrename = theatrename
+        self.morning_show = morning_show
+        self.movies = movies
+        self.evening_show = evening_show
 
 class tickets(db.Model):
     __tablename__='tickets'
